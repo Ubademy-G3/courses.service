8fe9ac28a10581a3b7781d78688bdcf32954554e
Daniela <daniela.leloutre@gmail.com>
2021-10-09 11:10:38 -0300

    quito tests, agrego pgadmin al docker-compose

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-from fastapi import Depends, FastAPI, HTTPException, status
-from sqlalchemy.orm.session import Session
-#from app.db import engine, database, metadata
-
-#metadata.create_all(engine)
+# from fastapi import Depends, FastAPI, HTTPException, status
+# from sqlalchemy.orm.session import Session
+# from typing import Iterator
+from fastapi import FastAPI
 
 app = FastAPI()
+
 
 @app.get("/ping")
 def pong():
     return {"ping": "pong!"}
-
-#@app.on_event("startup")
-#async def startup():
-#    await database.connect()
-
-
-#@app.on_event("shutdown")
-#async def shutdown():
-#    await database.disconnect()
