7d70e7b9a803bc6c13fb80f923275bd741233471
Dani Leloutre <39065230+dleloutre@users.noreply.github.com>
2021-10-09 16:11:29 -0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,11 @@
+until [ -f /etc/datadog-agent/datadog.yaml ]
+do
+  echo "Waiting for datadog.yaml to be generated"
+  sleep 1
+done
+
 datadog-agent run > /dev/null &
 /opt/datadog-agent/embedded/bin/trace-agent --config=/etc/datadog-agent/datadog.yaml > /dev/null &
 /opt/datadog-agent/embedded/bin/process-agent --config=/etc/datadog-agent/datadog.yaml > /dev/null &
+
+uvicorn main:app --host=0.0.0.0 --port=${PORT:-5000}
