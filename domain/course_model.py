31726b87784efc4b10a45072883c6536a5f97a1f
lautis1 <lautaro.stroia@gmail.com>
2021-10-22 00:00:34 -0300

    Added more fields to DB, modified schema and repos

--- domain/course_model.py
+++ domain/course_model.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel
+from uuid import uuid4, UUID
 
 
 class Course(BaseModel):
-    id: int
+    id: UUID = uuid4()
     name: str
+    description: str
+    hashtags: list
+    kind: str
+    subscription_type: str
+    location: str
