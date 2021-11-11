817b540d9dc4742be9295cfbdd4ed9bd2ea3ca7d
lautis1 <lautaro.stroia@gmail.com>
2021-11-11 19:48:35 -0300

    Added await to get

--- application/use_cases/course_media/get.py
+++ application/use_cases/course_media/get.py
@@ -3,9 +3,9 @@ from errors.http_error import NotFoundError
 
 cmrp = CourseMediaRepositoryPostgres()
 
-def get_all_course_media(course_id):
+async def get_all_course_media(course_id):
 
-    media_list = cmrp.get_all_course_media(course_id)
+    media_list = await cmrp.get_all_course_media(course_id)
     if media_list is None or len(media_list) == 0:
         raise NotFoundError("Media of course {}".format(course_id))
     return media_list
