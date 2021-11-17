6dc6b07337c92ae4e905d60f3135222a1fa9e6cd
lautis1 <lautaro.stroia@gmail.com>
2021-11-16 23:54:01 -0300

    Added tests with sessionmaker

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,29 @@
-'''import os
+import os
 import pytest
 from main import app
+from infrastructure.db.database import Base, get_db, engine
+from sqlalchemy.orm import sessionmaker
 from fastapi.testclient import TestClient
 
-@pytest.fixture(scope = "module")
+@pytest.fixture(scope = "class", autouse = False)
 def test_app():
     
     client = TestClient(app)
-    yield client'''
+    TestingSessionLocal = sessionmaker(autocommit = False,
+                                       autoflush = False,
+                                       bind = engine)
+    Base.metadata.create_all(bind = engine)
+
+    def get_testing_db():
+        db = TestingSessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
+    
+    app.dependency_overrides[get_db] = get_testing_db
+
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
+    yield client
