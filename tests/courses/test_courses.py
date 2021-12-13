b28f8579e72367b54b331a47bae09f0adf390981
lautis1 <lautaro.stroia@gmail.com>
2021-12-13 15:06:51 -0300

    Increasing cov

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -4,6 +4,8 @@ from tests.conftest import test_app
 from unittest import TestCase, mock
 from persistence.repositories.course_repository_postgres import CourseRepositoryPostgres
 from infrastructure.db.course_schema import Course
+from exceptions.auth_error import ApiKeyError
+from exceptions.http_error import NotFoundError
 
 header = {"apikey": os.getenv('API_KEY')}
 
@@ -23,6 +25,16 @@ test_request_payload = {
 }
 
 class CourseTest(TestCase):
+
+    @mock.patch.object(CourseRepositoryPostgres, "add_course")
+    def test_post_without_apikey(self, mock_method):
+
+        response = test_app.post("/courses/",
+                                data = json.dumps(test_request_payload))
+        response_json = response.json()
+
+        self.assertRaises(ApiKeyError)
+        assert response_json['message'] == "Error with API Key"
 
     @mock.patch.object(CourseRepositoryPostgres, "add_course")
     def test_create_course(self, mock_method):
@@ -48,6 +60,16 @@ class CourseTest(TestCase):
         assert response_json['language'] == test_request_payload['language']
         assert response_json['level'] == test_request_payload['level']
         assert response_json['modules'] == test_request_payload['modules']
+
+
+    @mock.patch.object(CourseRepositoryPostgres, "get_course_by_id")
+    def test_get_without_apikey(self, mock_method):
+
+        response = test_app.get("/courses/"+str(global_id))
+        response_json = response.json()
+
+        self.assertRaises(ApiKeyError)
+        assert response_json['message'] == "Error with API Key"
 
 
     ''' @mock.patch.object(CourseRepositoryPostgres, "get_course_by_id")
@@ -134,7 +156,31 @@ class CourseTest(TestCase):
             }]
         }'''
 
-    
+    @mock.patch.object(CourseRepositoryPostgres, "update_course")
+    @mock.patch.object(CourseRepositoryPostgres, "get_course_by_id")
+    def test_update_without_apikey(self, mock_method_update, mock_method_get):
+
+        test_patch_payload = {
+            "name": "Python 3",
+            "description": "Introduccion a ciencia de datos con python",
+            "category": 2,
+            "subscription_type": "free",
+            "location": "Argentina",
+            "profile_picture": "www.google.com",
+            "duration": 44,
+            "language": "spanish",
+            "level": "easy",
+            "modules": []
+        }
+        course_id = global_id
+        response = test_app.patch("/courses/"+str(course_id),
+                                data = json.dumps(test_patch_payload))
+        
+        response_json = response.json()
+        
+        self.assertRaises(ApiKeyError)
+        assert response_json['message'] == "Error with API Key"
+
     @mock.patch.object(CourseRepositoryPostgres, "update_course")
     @mock.patch.object(CourseRepositoryPostgres, "get_course_by_id")
     def test_update_course(self, mock_method_update, mock_method_get):
@@ -202,3 +248,17 @@ class CourseTest(TestCase):
         response_json = response.json()
         assert response.status_code == 200
         assert response_json['message'] == "Course {} deleted successfully".format(course_id)
+
+
+    @mock.patch.object(CourseRepositoryPostgres, "get_course_by_id")
+    def test_get_nonexistent_course(self, mock_method):
+        
+        course_id = global_id 
+        mock_method.return_value = None
+        
+        response = test_app.get("/courses/"+str(course_id), headers = header)
+        response_json = response.json()
+        
+        self.assertRaises(NotFoundError)
+        assert response_json['message'] == "Course {} not found".format(course_id)
+        
