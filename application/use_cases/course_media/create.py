69f837c76fe1d2c365f91b0fc83ddae52d7d38fc
lautis1 <lautaro.stroia@gmail.com>
2021-12-17 19:08:33 -0300

    Added checks module and media

--- application/use_cases/course_media/create.py
+++ application/use_cases/course_media/create.py
@@ -2,6 +2,8 @@ from persistence.repositories.course_media_repository_postgres import CourseMedi
 from infrastructure.db.course_media_schema import CourseMedia
 from application.serializers.course_media_serializer import CourseMediaSerializer
 from uuid import uuid4
+from exceptions.http_error import NotFoundError
+from application.use_cases.course_module import get as module_get
 
 cmrp = CourseMediaRepositoryPostgres()
 
@@ -9,6 +11,10 @@ cmrp = CourseMediaRepositoryPostgres()
 def add_course_media(db, course_id, args):
 
     new_course_media = CourseMedia(id=uuid4(), course_id=course_id, module_id=args.module_id, url=args.url)
+
+    module = module_get.get_module(db, args.module_id)
+    if not module:
+        raise NotFoundError("Module")
 
     cmrp.add_course_media(db, new_course_media)
     return CourseMediaSerializer.serialize(new_course_media)
--- application/use_cases/course_module/create.py
+++ application/use_cases/course_module/create.py
@@ -2,6 +2,8 @@ from persistence.repositories.course_module_repository_postgres import CourseMod
 from infrastructure.db.course_module_schema import CourseModule
 from application.serializers.course_module_serializer import CourseModuleSerializer
 from uuid import uuid4
+from application.use_cases.course.get import course_exists
+from exceptions.http_error import NotFoundError
 
 cmorp = CourseModuleRepositoryPostgres()
 
@@ -9,6 +11,7 @@ cmorp = CourseModuleRepositoryPostgres()
 def add_module(db, course_id, args):
 
     new_module = CourseModule(id=uuid4(), course_id=course_id, title=args.title, content=args.content)
-
+    if not course_exists(db, course_id):
+        raise NotFoundError("Course")
     cmorp.add_module(db, new_module)
     return CourseModuleSerializer.serialize(new_module)
